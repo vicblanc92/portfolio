797cbd7588fd9c776d88d50cb0314c6714e59526
vblanc <vicbr92@gmail.com>
2022-04-21 19:26:35 +0200

    add App component

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import Aboutme from './Aboutme';
+import Header from './Header';
+import Technologies from './Technologies';
+import Projects from './Projects';
+import Footer from './Footer';
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Aboutme />
+      <Technologies />
+      <Projects />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
