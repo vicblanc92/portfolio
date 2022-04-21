0842d2bb349542f0b88fd7834540f20d39b4eb88
vblanc <vicbr92@gmail.com>
2022-04-21 19:26:50 +0200

    create Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,51 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+const Footer = () => {
+  return (
+    <>
+      <section className="footer">
+        <nav className="footer__nav">
+          <ul className="footer__ul">
+            <div className="footer__title">
+              <li>
+                <strong>Contacto</strong>
+              </li>
+            </div>
+            <div className="footer__items">
+              <li className="footer__item">
+                <a
+                  className="footer__item"
+                  href="https://linkedin.com/in/victoria-blanc"
+                >
+                  <FontAwesomeIcon icon={faLinkedin} />{' '}
+                </a>
+              </li>
+
+              <li className="footer__item">
+                <a
+                  className="footer__item"
+                  href="https://twitter.com/BlancRastoll"
+                >
+                  <FontAwesomeIcon icon={faTwitter} />
+                </a>
+              </li>
+              <li className="footer__item">
+                <a
+                  className="footer__item"
+                  href="https://github.com/vicblanc92"
+                >
+                  <FontAwesomeIcon icon={faGithub} />
+                </a>
+              </li>
+            </div>
+          </ul>
+        </nav>
+      </section>
+    </>
+  );
+};
+
+export default Footer;
