6fe1263d71cddef0c54f84c6285081b9d911dedd
vblanc <vicbr92@gmail.com>
2022-04-22 10:00:07 +0200

    update Aboutme component

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -11,13 +11,14 @@ const Aboutme = () => {
         <article className="main__aboutme">
           <h2 className="main__aboutme--title">Sobre mí</h2>
           <h3 className="main__aboutme--text">
-            Mi nombre es Victoria, y soy desarrolladora Junior Frontend. Tras
-            años dedicándome al sector educativo, decidí darle un giro
-            profesional a mi vida y enfrentarme a nuevos retos en el mundo
-            tecnológico. Así fue como comencé mi aventura en Adalab, bootcamp en
-            el que descubrí que me encantaría dedicarme a este sector y seguir
-            aprendiendo nuevas tecnologías a parte de las nociones que ya
-            aprendí en el bootcamp. Mi objetivo es poder seguir aprendiendo y
+            Mi nombre es <u>Victoria</u>, y soy{' '}
+            <u>desarrolladora Junior Frontend</u>. Tras años dedicándome al
+            sector educativo, decidí darle un giro profesional a mi vida y
+            enfrentarme a nuevos retos en el <u>mundo tecnológico</u>. Así fue
+            como comencé mi aventura en Adalab, bootcamp en el que descubrí que
+            me encantaría dedicarme a este sector y seguir aprendiendo{' '}
+            <u>nuevas tecnologías</u> a parte de las nociones que ya aprendí en
+            el bootcamp. Mi <u>objetivo</u> es poder seguir aprendiendo y
             formándome en entornos de trabajo que me permitan seguir alimentando
             mi curiosidad.
           </h3>
